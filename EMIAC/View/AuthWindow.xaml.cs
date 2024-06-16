e03f849c7af36fcd16f0fb1bb7603c15c48a6dfb
siittance <144900566+siittance@users.noreply.github.com>
2024-06-16 16:20:58 +0300

    Попытка

--- EMIAC/View/AuthWindow.xaml.cs
+++ EMIAC/View/AuthWindow.xaml.cs
@@ -29,5 +29,69 @@ namespace EMIAC.View
             authsotr.Show();
             this.Close();
         }
+
+        private void Exit(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Svernyt(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void FullEkran(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Normal;
+            }
+            else
+            {
+                this.WindowState = WindowState.Maximized;
+            }
+        }
+        private Point _mouseDownPosition;
+        private bool _isMouseDown;
+
+        protected override void OnMouseDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseDown(e);
+
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                _isMouseDown = true;
+                _mouseDownPosition = e.GetPosition(this);
+            }
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+
+            if (_isMouseDown)
+            {
+                Point currentPosition = e.GetPosition(this);
+                this.Left += currentPosition.X - _mouseDownPosition.X;
+                this.Top += currentPosition.Y - _mouseDownPosition.Y;
+            }
+        }
+
+        protected override void OnMouseUp(MouseButtonEventArgs e)
+        {
+            base.OnMouseUp(e);
+
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                _isMouseDown = false;
+            }
+        }
+
+        private void Enter_User(object sender, RoutedEventArgs e)
+        {
+            MainUserWindow mainUserWindow = new MainUserWindow();
+            mainUserWindow.Show();
+            this.Close();
+        }
     }
 }
