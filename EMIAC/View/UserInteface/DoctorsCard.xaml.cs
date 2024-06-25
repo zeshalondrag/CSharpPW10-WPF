d1c50546450331947eebb4954afb23d05856e178
siittance <144900566+siittance@users.noreply.github.com>
2024-06-26 01:04:10 +0300

    Добавление карточки врача и удаление всех карточек с врачами из xaml

---
+++ EMIAC/View/UserInteface/DoctorsCard.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EMIAC.View.UserInteface
+{
+    /// <summary>
+    /// Логика взаимодействия для DoctorsCard.xaml
+    /// </summary>
+    public partial class DoctorsCard : UserControl
+    {
+        public DoctorsCard()
+        {
+            InitializeComponent();
+        }
+    }
+}
