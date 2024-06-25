6fffacadfc620465985daf9fe27447355c72ac8a
siittance <144900566+siittance@users.noreply.github.com>
2024-06-25 12:08:56 +0300

    Пользовательский интерфейс

--- EMIAC/View/MainUserWindow.xaml.cs
+++ EMIAC/View/MainUserWindow.xaml.cs
@@ -23,6 +23,18 @@ namespace EMIAC.View
         public MainUserWindow()
         {
             InitializeComponent();
+            MainDobavCard card = new MainDobavCard();
+            MainDobavCard card1 = new MainDobavCard();
+            MainDobavCard card2 = new MainDobavCard();
+            MainDobavCard card3 = new MainDobavCard();
+            MainDobavCard card4 = new MainDobavCard();
+            MainDobavCard card5 = new MainDobavCard();
+            DobavAktiv.Items.Add(card);
+            DobavAktiv.Items.Add(card1);
+            DobavAktiv1.Items.Add(card2);
+            DobavArxiv.Items.Add(card3);
+            DobavArxiv.Items.Add(card4);
+            DobavArxiv1.Items.Add(card5);
             this.WindowState = WindowState.Maximized;
         }
 
---
+++ EMIAC/View/UserInterface/VrachWindowZapisi.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EMIAC.View.UserInterface
+{
+    /// <summary>
+    /// Логика взаимодействия для VrachWindowZapisi.xaml
+    /// </summary>
+    public partial class VrachWindowZapisi : UserControl
+    {
+        public VrachWindowZapisi()
+        {
+            InitializeComponent();
+        }
+    }
+}
