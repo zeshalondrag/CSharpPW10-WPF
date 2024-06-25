5f172253d6027db21d34e0cd02e6f5535ce5b75d
Timok951 <timofey.alekseev@list.ru>
2024-06-25 23:29:57 +0300

    Second push

--- EMIAC/ViewModel/PatientViewModelSettings.cs
+++ EMIAC/ViewModel/PatientViewModelSettings.cs
@@ -6,35 +6,144 @@ using System.Threading.Tasks;
 
 namespace calculator.ViewModel.Helpers
 {
-    internal class BindableCommand : ICommand
+    using System;
+    using System.Collections.ObjectModel;
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+    using System.Threading.Tasks;
+    using System.Windows.Input;
+    using System.Linq;
+    using calculator.ViewModel.Helpers;
+    using EMIAC.ViewModel.Helpers;
+    using EMIAC.View;
+    using System.Windows;
+
+    namespace EMIAC.ViewModel
     {
-        private Action<object> _execute;
-        private Func<object, bool> _canExecute;
-
-
-        public event EventHandler CanExecuteChanged
+        internal class PatientViewModelSettings : BindingHelper
         {
-            add { CommandManager.RequerySuggested += value; }
-            remove { CommandManager.RequerySuggested -= value; }
-        }
+            private readonly ApiService _apiService;
+            private string _patientName;
+            private string _patientEmail;
+            private string _patientphonenumber;
+            private int _patientId;
+            private string _patientadress;
+            private string _patientlivingadress;
+            private long _patientoms;
+            private DateTime _patientdateofbirth;
+
+            public PatientViewModelSettings(int patientId)
+            {
+                _apiService = new ApiService();
+                _patientId = patientId;
+                InitializePatient();
+            }
 
 
-        public BindableCommand(Action<object> execute, Func<object, bool> canExecute = null)
-        {
-            _execute = execute;
-            _canExecute = canExecute;
-        }
+            public string PatientName
+            {
+                get => _patientName;
+                set
+                {
+                    _patientEmail = value;
+                    OnPropertyChanged();
+                }
+            }
+
+            public string PatientEmail
+            {
+                get => _patientEmail;
+                set
+                {
+                    _patientEmail = value;
+                    OnPropertyChanged();
+                }
+            }
+
+            public string PatientPhoneNumber
+            {
+                get => _patientphonenumber;
+                set
+                {
+                    _patientphonenumber = value;
+                    OnPropertyChanged();
+                }
+            }
+
+            public string PatientAdress
+            {
+                get => _patientadress;
+                set
+                {
+                    _patientphonenumber = value;
+                    OnPropertyChanged();
+                }
+            }
+
+            public string PatientLivingAdress
+            {
+                get => _patientlivingadress;
+                set
+                {
+                    _patientlivingadress = value;
+                    OnPropertyChanged();
+                }
+            }
+
+            public long PatientOms
+            {
+                get => _patientoms;
+                set
+                {
+                    _patientoms = value;
+                    OnPropertyChanged();
+                }
+            }
+
+            public DateTime PatientDateOfBirth
+            {
+                get => _patientdateofbirth;
+                set
+                {
+                    _patientdateofbirth = value;
+                    OnPropertyChanged();
+                }
+            }
+
+            public BindableCommand AddCommand { get; set; }
 
 
-        public bool CanExecute(object parameter)
-        {
-            return _canExecute == null || _canExecute(parameter);
-        }
+            private async void InitializePatient()
+            {
+                var patient = await _apiService.GetPatientById(_patientId);
+
+                PatientName = $" {patient.PatientName} {patient.PatientSurname} {patient.PatientNickName}";
+                PatientOms = patient.OMS;
+                PatientDateOfBirth = patient.PatientBirthDate;
+            }
+
+            private async Task SaveChanges()
+            {
+                await _apiService.UpdatePatient(_patientId, PatientPhoneNumber, PatientEmail, PatientAdress, PatientLivingAdress);
+
+            }
+
+            private void ExitfromAccount()
+            {
+                AuthWindow authclient = new AuthWindow();
+                Application.Current.MainWindow.Close();
+                authclient.Show();
+
+            }
 
 
-        public void Execute(object parameter)
-        {
-            _execute(parameter);
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+
         }
     }
 
